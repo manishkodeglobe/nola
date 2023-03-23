0e82ce351b77e527fb71bb86646f7e72c96f951d
manish verma <mnu.verma77@gmail.com>
2023-03-23 12:26:51 +0530

    manish

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './template/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
